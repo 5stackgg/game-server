7ab682d6b688399de36aaeda1ff79ccad4a25c0a
Luke Policinski <luke@lukepolo.com>
2023-11-06 15:29:04 -0500

    hell yes

--- PlayCS/HelloWorldPlugin.cs
+++ PlayCS/HelloWorldPlugin.cs
@@ -1,14 +1,70 @@
-﻿using CounterStrikeSharp.API.Core;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Cvars;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Modules.Timers;
+using CounterStrikeSharp.API.Modules.Utils;
+
 
 namespace HelloWorldPlugin;
 public class HelloWorldPlugin : BasePlugin
 {
-    public override string ModuleName => "Hello World Plugin";
+    public override string ModuleName => "PlayCS Mod";
 
     public override string ModuleVersion => "0.0.1";
 
     public override void Load(bool hotReload)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine(
+            $"Test Plugin has been loaded, and the hot reload flag was {hotReload}, path is {ModulePath}");    
+        // Message(HudDestination.Center, message);
+    }
+   
+    
+    [ConsoleCommand("custom_command", "Sends a message to the server")]
+    public void OnCommand(CCSPlayerController? player, CommandInfo command)
+    {
+        Message(HudDestination.Chat, "{RED}Hello{RED}");
+    }
+    
+    private void Message(HudDestination destination, string message)
+    {
+        message = ReplaceColorTags(message);
+
+        if (destination != HudDestination.Center)
+        {
+            var parts = message.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            foreach (var part in parts)
+                Server.PrintToChatAll($" {part}");
+        }
+        else
+            VirtualFunctions.ClientPrintAll(destination, $" {message}", 0, 0, 0, 0);
+    }
+    
+
+    private string ReplaceColorTags(string input)
+    {
+        string[] colorPatterns =
+        {
+            "{DEFAULT}", "{RED}", "{LIGHTPURPLE}", "{GREEN}", "{LIME}", "{LIGHTGREEN}", "{LIGHTRED}", "{GRAY}",
+            "{LIGHTOLIVE}", "{OLIVE}", "{LIGHTBLUE}", "{BLUE}", "{PURPLE}", "{GRAYBLUE}"
+        };
+        string[] colorReplacements =
+        {
+            "\x01", "\x02", "\x03", "\x04", "\x05", "\x06", "\x07", "\x08", "\x09", "\x10", "\x0B", "\x0C", "\x0E",
+            "\x0A"
+        };
+
+        for (var i = 0; i < colorPatterns.Length; i++)
+            input = input.Replace(colorPatterns[i], colorReplacements[i]);
+
+        return input;
     }
 }
