aa524726e6beac116572daa2187eec265f19a20a
Luke Policinski <luke@lukepolo.com>
2023-11-06 15:12:19 -0500

    feature/poc

---
+++ PlayCS/HelloWorldPlugin.cs
@@ -0,0 +1,14 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace HelloWorldPlugin;
+public class HelloWorldPlugin : BasePlugin
+{
+    public override string ModuleName => "Hello World Plugin";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
