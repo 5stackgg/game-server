439ed95d85ea14a6130a3d41ee110520bdc9bba7
Luke Policinski <luke@lukepolo.com>
2023-11-09 10:50:18 -0500

    wip

--- PlayCS/PlayCsPlugin.cs
+++ PlayCS/PlayCsPlugin.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
+﻿using System.Text.Json;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
