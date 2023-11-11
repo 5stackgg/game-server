1fc27ebf0bf2de402f5492107d79ae9e3c0c4c85
Luke Policinski <luke@lukepolo.com>
2023-11-11 10:08:34 -0500

    wip

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
-VERSION=$(grep '<Version>' ./PlayCS/PlayCS.csproj | sed -n 's/.*<Version>\(.*\)<\/Version>.*/\1/p')
+VERSION=$(grep '<Version>' ./PlayCS.csproj | sed -n 's/.*<Version>\(.*\)<\/Version>.*/\1/p')
 
 docker build -t lukepolo/playcs-server:beta-1 --platform linux/amd64 .
