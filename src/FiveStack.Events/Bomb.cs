aa3fb483ab808362f15bc46764b60c7bba22cfdc
Luke Policinski <luke@lukepolo.com>
2024-03-14 16:31:32 -0400

    wip

--- src/FiveStack.Events/Bomb.cs
+++ src/FiveStack.Events/Bomb.cs
@@ -13,8 +13,23 @@ public partial class FiveStackPlugin
             return HookResult.Continue;
         }
 
-        CCSPlayerController attacked = @event.Userid;
+        CCSPlayerController planter = @event.Userid;
 
+        _redis.PublishMatchEvent(
+            _matchData.id,
+            new Redis.EventData<Dictionary<string, object>>
+            {
+                @event = "bomb",
+                data = new Dictionary<string, object>
+                {
+                    { "match_map_id", _matchData.current_match_map_id },
+                    { "round", _currentRound + 1 },
+                    { "type", "plant" },
+                    { "player_steam_id", planter.SteamID.ToString() },
+                }
+            }
+        );
+        
         return HookResult.Continue;
     }
 
@@ -26,7 +41,50 @@ public partial class FiveStackPlugin
             return HookResult.Continue;
         }
 
-        CCSPlayerController attacked = @event.Userid;
+        CCSPlayerController defuser = @event.Userid;
+
+        _redis.PublishMatchEvent(
+            _matchData.id,
+            new Redis.EventData<Dictionary<string, object>>
+            {
+                @event = "bomb",
+                data = new Dictionary<string, object>
+                {
+                    { "match_map_id", _matchData.current_match_map_id },
+                    { "round", _currentRound + 1 },
+                    { "type", "plant" },
+                    { "player_steam_id", defuser.SteamID.ToString() },
+                }
+            }
+        );
+
+        return HookResult.Continue;
+    }
+
+     [GameEventHandler]
+    public HookResult BombExploded(EventBombExploded @event, GameEventInfo info)
+    {
+        if (@event.Userid == null || !@event.Userid.IsValid || _matchData == null || !IsLive())
+        {
+            return HookResult.Continue;
+        }
+
+        CCSPlayerController bomber = @event.Userid;
+
+        _redis.PublishMatchEvent(
+            _matchData.id,
+            new Redis.EventData<Dictionary<string, object>>
+            {
+                @event = "bomb",
+                data = new Dictionary<string, object>
+                {
+                    { "match_map_id", _matchData.current_match_map_id },
+                    { "round", _currentRound + 1 },
+                    { "type", "exploded" },
+                    { "player_steam_id", bomber.SteamID.ToString() },
+                }
+            }
+        );
 
         return HookResult.Continue;
     }
