b4890a1d0a83d0d13830aa001bcc89d0a453719d
Luke Policinski <luke@lukepolo.com>
2024-04-10 18:18:17 -0400

    wip

--- src/FiveStack.Events/Spectator.cs
+++ src/FiveStack.Events/Spectator.cs
@@ -2,6 +2,7 @@ using System.Runtime.InteropServices;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
 using CounterStrikeSharp.API.Modules.Utils;
+using FiveStack.Entities;
 using FiveStack.Utilities;
 using Microsoft.Extensions.Logging;
 
@@ -28,14 +29,30 @@ public partial class FiveStackPlugin
 
     private HookResult SpectatorChangedHook(DynamicHook handle)
     {
+        MatchManager? match = _matchService.GetCurrentMatch();
+        MatchData? matchData = match?.GetMatchData();
+
+        if (matchData == null)
+        {
+            return HookResult.Continue;
+        }
+
         var observerServices = handle.GetParam<CPlayer_ObserverServices>(0);
 
         ObserverMode_t desiredMode = ObserverMode_t.OBS_MODE_IN_EYE;
 
-        ulong? spectator = observerServices.Pawn.Value.Controller.Value?.SteamID;
+        CBasePlayerController? spectator = observerServices.Pawn.Value.Controller.Value;
+
+        if (spectator == null)
+        {
+            return HookResult.Continue;
+        }
+
+        var player = CounterStrikeSharp.API.Utilities.GetPlayerFromSteamId(spectator.SteamID);
+
         int viewingTeamNum = observerServices.ObserverTarget.Value?.TeamNum ?? -1;
 
-        if (spectator == null || viewingTeamNum == -1)
+        if (player == null || viewingTeamNum == -1)
         {
             return HookResult.Continue;
         }
@@ -43,21 +60,47 @@ public partial class FiveStackPlugin
         CsTeam viewingTeam = TeamUtility.TeamNumToCSTeam(viewingTeamNum);
 
         _logger.LogInformation(
-            $"Specator ID: {spectator} is viewing {viewingTeam} @ {observerServices.ObserverMode}"
+            $"Specator ID: {spectator.SteamID} is viewing {viewingTeam} @ {observerServices.ObserverMode}"
         );
 
         if (desiredMode != (ObserverMode_t)observerServices.ObserverMode)
         {
             _logger.LogWarning($"BAD SPECTATOR");
+            // TODO - chnage their view to eyes
         }
 
-        _logger.LogWarning($"BAD SPECTATOR2");
+        MatchMember? member = MatchUtility.GetMemberFromLineup(matchData, player);
+        Guid? lineup_id = MatchUtility.GetPlayerLineup(matchData, player);
 
-        if(viewingTeam == CsTeam.None || viewingTeam == CsTeam.Spectator || viewingTeam != CsTeam.Terrorist) {
+        if (member == null || lineup_id == null)
+        {
+            return HookResult.Continue;
+        }
+
+        CsTeam expectedTeam = CsTeam.None;
+
+        string lineupName =
+            matchData.lineup_1_id == lineup_id ? matchData.lineup_1.name : matchData.lineup_2.name;
+
+        foreach (var team in MatchUtility.Teams())
+        {
+            if (team.ClanTeamname == lineupName)
+            {
+                expectedTeam = TeamUtility.TeamNumToCSTeam(team.TeamNum);
+            }
+        }
+
+        if (expectedTeam == CsTeam.None)
+        {
+            _logger.LogWarning("Unable to get expected team");
+            return HookResult.Continue;
+        }
+
+        if (viewingTeam != expectedTeam)
+        {
             _logger.LogWarning("BAD VIEW");
-            // return HookResult.Stop;
+            // TODO - we may be able todo this in PRE mode instead, otherwise we have to change it after they view that person
         }
-
 
         return HookResult.Continue;
     }
