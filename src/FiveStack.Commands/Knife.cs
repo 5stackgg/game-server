a073d3b0ed5a972a7f6815f2736d33ef0b36c697
Luke Policinski <Luke@LukePOLO.com>
2024-08-14 22:35:21 -0400

    bug: fix demo uploading

--- src/FiveStack.Commands/Knife.cs
+++ src/FiveStack.Commands/Knife.cs
@@ -1,6 +1,7 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
+using Microsoft.Extensions.Logging;
 
 namespace FiveStack;
 
@@ -40,7 +41,7 @@ public partial class FiveStackPlugin
     {
         MatchManager? match = _matchService.GetCurrentMatch();
 
-        if (player == null || match == null)
+        if (match == null)
         {
             return;
         }
