966b36061f0aa8cdbf896f4801be95e08079e819
Luke Policinski <Luke@LukePOLO.com>
2024-10-15 11:26:19 -0400

    wip

--- src/FiveStackPlugin.cs
+++ src/FiveStackPlugin.cs
@@ -21,7 +21,7 @@ public partial class FiveStackPlugin : BasePlugin
     private readonly EnvironmentService _environmentService;
 
     public override string ModuleName => "FiveStack";
-    public override string ModuleVersion => "0.0.1";
+    public override string ModuleVersion => "__RELEASE_VERSION__";
     public override string ModuleAuthor => "5Stack.gg";
     public override string ModuleDescription => "5Stack creates and managements custom matches";
 
