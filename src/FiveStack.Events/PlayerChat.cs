708bd397b4d5fc6fb4235bdde1691260e45e8b73
Luke Policinski <Luke@LukePOLO.com>
2024-11-14 14:07:46 -0500

    bug: dont send chat to web

--- src/FiveStack.Events/PlayerChat.cs
+++ src/FiveStack.Events/PlayerChat.cs
@@ -1,5 +1,7 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Commands;
+using FiveStack.Entities;
+using FiveStack.Utilities;
 
 namespace FiveStack;
 
@@ -17,6 +19,23 @@ public partial class FiveStackPlugin
         if (match == null)
         {
             return HookResult.Continue;
+        }
+
+        MatchData? matchData = match.GetMatchData();
+
+        if (matchData == null)
+        {
+            return HookResult.Continue;
+        }
+
+        MatchMember? member = MatchUtility.GetMemberFromLineup(matchData, player);
+
+        if (member != null)
+        {
+            if (member.is_gagged)
+            {
+                return HookResult.Stop;
+            }
         }
 
         _matchEvents.PublishGameEvent(
