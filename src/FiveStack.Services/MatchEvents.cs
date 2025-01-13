fedc640d705e7f7aa916a8e2e643f8137ba71472
Luke Policinski <luke@lukepolo.com>
2025-01-13 12:29:45 -0500

    chore: wip

--- src/FiveStack.Services/MatchEvents.cs
+++ src/FiveStack.Services/MatchEvents.cs
@@ -118,7 +118,6 @@ public class MatchEvents
                 if (result.MessageType == WebSocketMessageType.Text)
                 {
                     var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
-                    _logger.LogInformation($"Received message: {message}");
                     try
                     {
                         var response = JsonSerializer.Deserialize<FiveStackMessageResponse>(
