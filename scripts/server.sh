7b879d61ffbc50cfc640ac80438000bd203c41fb
Luke Policinski <luke@lukepolo.com>
2025-05-12 13:42:57 -0400

    chore: remove default map

--- scripts/server.sh
+++ scripts/server.sh
@@ -6,4 +6,4 @@ cp -R "${STEAMCMD_DIR}/linux64/"* "/root/.steam/sdk64/"
 
 echo "---Starting Server...--"
 cd ${INSTANCE_SERVER_DIR}
-${INSTANCE_SERVER_DIR}/game/bin/linuxsteamrt64/cs2 -ip 0.0.0.0 -port ${SERVER_PORT} +tv_port ${TV_PORT} -dedicated -dev +map ${DEFAULT_MAP} -usercon +rcon_password ${RCON_PASSWORD} +sv_password ${SERVER_PASSWORD} ${EXTRA_GAME_PARAMS}
+${INSTANCE_SERVER_DIR}/game/bin/linuxsteamrt64/cs2 -ip 0.0.0.0 -port ${SERVER_PORT} +tv_port ${TV_PORT} -dedicated -dev -usercon +rcon_password ${RCON_PASSWORD} +sv_password ${SERVER_PASSWORD} ${EXTRA_GAME_PARAMS}
