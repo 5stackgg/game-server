190d062354d3370f2f4617f1c07e869f36a5db2f
Luke Policinski <Luke@LukePOLO.com>
2025-08-03 23:28:19 -0400

    chore: update sig

--- src/FiveStack.Events/InitConnect.cs
+++ src/FiveStack.Events/InitConnect.cs
@@ -18,7 +18,7 @@ public partial class FiveStackPlugin
 
     // near "CNetworkGameServerBase::ConnectClient( name=\'%s\', remote=\'%s\' )\n"
     private static string ConnectClientSignature = RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
-        ? "55 48 89 E5 41 57 41 56 41 89 CE 41 55 41 54 4D 89 CC 53 48 89 D3 48 81 EC F8 03 00 00 8B 45 20 48 89 BD 48 FC FF FF 48 8B 3D ?? ?? ?? ?? 48 89 B5 40 FC FF FF 48 C7 85 60 FC FF FF 00 00 00 00 89 8D 30 FC FF FF 89 85 10 FC FF FF 48 8B 07 4C 89 85 38 FC FF FF FF 90 60 01 00 00 44 89 F6 48 89 C7 48 8D 85 10 FD FF FF 48 89 C2 48 89 85 28 FC FF FF 48 8B 07 FF 50 78 84 C0 0F 84 ?? ?? ?? ??"
+        ? "55 48 89 E5 41 57 41 56 41 89 CE 41 55 41 54 4D 89 CC 53 48 89 D3 48 81 EC F8 03 00 00 8B 45 20 48 89 BD 48 FC FF FF 48 8B 3D ?? ?? ?? ?? 48 89 B5 40 FC FF FF 48 C7 85 60 FC FF FF 00 00 00 00"
         : "";
     /// <summary>
     /// <c>
