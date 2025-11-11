2bb44cb87616813f501c3fd65bdcde47fb7dc9a0
Luke Policinski <luke@lukepolo.com>
2025-11-11 14:42:11 -0500

    bug: revert going down a round

--- src/FiveStack.Commands/BackupRounds.cs
+++ src/FiveStack.Commands/BackupRounds.cs
@@ -25,7 +25,7 @@ public partial class FiveStackPlugin
             return;
         }
 
-        _gameBackupRounds.SendRestoreRoundToBackend(round - 1);
+        _gameBackupRounds.SendRestoreRoundToBackend(round);
     }
 
     [ConsoleCommand(
@@ -77,6 +77,6 @@ public partial class FiveStackPlugin
             return;
         }
 
-        _gameBackupRounds.RequestRestoreBackupRound(round - 1, player);
+        _gameBackupRounds.RequestRestoreBackupRound(round, player);
     }
 }
