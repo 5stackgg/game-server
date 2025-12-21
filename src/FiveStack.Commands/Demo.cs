0985c332fe1a6c9537916d5bf69dcd76b24dee83
Luke Policinski <luke@lukepolo.com>
2025-12-21 16:44:44 -0500

    bug: fix translation for pt-BR

--- src/FiveStack.Commands/Demo.cs
+++ src/FiveStack.Commands/Demo.cs
@@ -1,6 +1,7 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Commands;
+using FiveStack.Enums;
 
 namespace FiveStack;
 
@@ -10,17 +11,25 @@ public partial class FiveStackPlugin
     [CommandHelper(whoCanExecute: CommandUsage.SERVER_ONLY)]
     public async void OnUploadDemo(CCSPlayerController? player, CommandInfo command)
     {
+         MatchManager? match = _matchService.GetCurrentMatch();
+        if (match == null)
+        {
+            return;
+        }
+
+        match.UpdateMapStatus(eMapStatus.UploadingDemo);
+
         await _gameDemos.UploadDemos();
     }
 
-    [ConsoleCommand("start_demo", "start demo recording")]
+    [ConsoleCommand("test_start_demo", "start demo recording")]
     [CommandHelper(whoCanExecute: CommandUsage.SERVER_ONLY)]
     public void StartDemo(CCSPlayerController? player, CommandInfo command)
     {
         _gameDemos.Start();
     }
 
-    [ConsoleCommand("stop_demo", "stop demo recording")]
+    [ConsoleCommand("test_stop_demo", "stop demo recording")]
     [CommandHelper(whoCanExecute: CommandUsage.SERVER_ONLY)]
     public void StopDemo(CCSPlayerController? player, CommandInfo command)
     {
