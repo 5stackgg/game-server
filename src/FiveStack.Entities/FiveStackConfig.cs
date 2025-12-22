f77d019b1b0f9831c330293e3a65e998dfb38724
Luke Policinski <luke@lukepolo.com>
2025-12-22 17:31:26 -0500

    chore: update to tv

--- src/FiveStack.Entities/FiveStackConfig.cs
+++ src/FiveStack.Entities/FiveStackConfig.cs
@@ -5,7 +5,7 @@ public class FiveStackConfig : IBasePluginConfig
     public int Version { get; set; } = 1;
     public string WS_DOMAIN { get; set; } = "wss://ws.5stack.gg";
     public string API_DOMAIN { get; set; } = "https://api.5stack.gg";
-    public string RELAY_DOMAIN { get; set; } = "https://relay.5stack.gg";
+    public string RELAY_DOMAIN { get; set; } = "https://tv.5stack.gg";
     public string DEMOS_DOMAIN { get; set; } = "https://demos.5stack.gg";
     public string SERVER_ID { get; set; } = "";
     public string SERVER_API_PASSWORD { get; set; } = "";
